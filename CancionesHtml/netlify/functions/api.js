fc3ecb55f6303d9b8dcba8d0b0d81cf189fc9459
laureanobonilla <bonillapretiz@gmail.com>
2026-09-16 17:29:13 -0600

    Add files via upload

--- CancionesHtml/netlify/functions/api.js
+++ CancionesHtml/netlify/functions/api.js
@@ -2,22 +2,46 @@ const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
     const MASTER_KEY = process.env.JSONBIN_MASTER_KEY;
-    const { endpoint, method, body } = JSON.parse(event.body || '{}');
+    
+    if (!MASTER_KEY) {
+        return { statusCode: 500, body: JSON.stringify({ error: "Master Key no configurada en el servidor" }) };
+    }
 
     try {
-        const response = await fetch(`https://api.jsonbin.io/v3/${endpoint}`, {
-            method: method || 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Master-Key': MASTER_KEY
-            },
-            body: body ? JSON.stringify(body) : undefined
-        });
+        const { action, binId, data, binName } = JSON.parse(event.body || '{}');
+        let url = "https://api.jsonbin.io/v3/b";
+        let method = "GET";
+        let body = undefined;
 
-        const data = await response.json();
+        if (action === 'create') {
+            method = "POST";
+            body = JSON.stringify(data);
+        } else if (action === 'update') {
+            method = "PUT";
+            url = `https://api.jsonbin.io/v3/b/${binId}`;
+            body = JSON.stringify(data);
+        } else if (action === 'get') {
+            method = "GET";
+            url = `https://api.jsonbin.io/v3/b/${binId}/latest`;
+        } else if (action === 'list') {
+            method = "GET";
+            url = "https://api.jsonbin.io/v3/b?meta=false";
+        }
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'X-Master-Key': MASTER_KEY
+        };
+        if (binName && action === 'create') {
+            headers['X-Bin-Name'] = binName;
+        }
+
+        const response = await fetch(url, { method, headers, body });
+        const jsonResponse = await response.json();
+
         return {
             statusCode: response.status,
-            body: JSON.stringify(data)
+            body: JSON.stringify(jsonResponse)
         };
     } catch (error) {
         return {
