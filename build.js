13155efce98cf0ece67a173cc7ac8881b5372275
laureanobonilla <bonillapretiz@gmail.com>
2026-09-16 18:26:50 -0600

    Update build.js

--- build.js
+++ build.js
@@ -8,7 +8,7 @@ const JSONBIN_INDICE_ID = process.env.JSONBIN_INDICE_ID || '';
 // Procesar index.html
 if (fs.existsSync('index.html')) {
     let html = fs.readFileSync('index.html', 'utf8');
-    html = html.replace(/__GEMINI_API_KEY__/g, GEMINI_API_KEY);
+    html = html.replace(/__CLAVE_GEMINI__/g, GEMINI_API_KEY);
     html = html.replace(/__JSONBIN_MASTER_KEY__/g, JSONBIN_MASTER_KEY);
     html = html.replace(/__JSONBIN_INDICE_ID__/g, JSONBIN_INDICE_ID);
     fs.writeFileSync('index.html', html);
@@ -17,7 +17,7 @@ if (fs.existsSync('index.html')) {
 // Procesar admin.html
 if (fs.existsSync('admin.html')) {
     let html = fs.readFileSync('admin.html', 'utf8');
-    html = html.replace(/__GEMINI_API_KEY__/g, GEMINI_API_KEY);
+    html = html.replace(/__CLAVE_GEMINI__/g, GEMINI_API_KEY);
     html = html.replace(/__JSONBIN_MASTER_KEY__/g, JSONBIN_MASTER_KEY);
     html = html.replace(/__JSONBIN_INDICE_ID__/g, JSONBIN_INDICE_ID);
     fs.writeFileSync('admin.html', html);
